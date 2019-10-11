9e9d7525e1ef9c2e31a2e1e6f2a05e27b48a13b6
liyuping02 <liyuping02@sunlands.com>
2019-10-11 12:36:25 +0800

    开发博客列表路由

---
+++ blog-1/src/controller/blog.js
@@ -0,0 +1,22 @@
+const getList = (author, keyword) => {
+    // 先返回假数据（格式是正确的）
+    return [
+        {
+            id: 1,
+            title: '标题A',
+            content: '内容A',
+            createTime: 1570768216454,
+            author: 'zhangsan'
+        }, {
+            id: 2,
+            title: '标题B',
+            content: '内容B',
+            createTime: 1570768123456,
+            author: 'lisi'
+        }
+    ]
+}
+
+module.exports = {
+    getList
+}

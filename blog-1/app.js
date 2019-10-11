d10e508718de7da6ba9888043282e8fda7709007
liyuping02 <liyuping02@sunlands.com>
2019-10-11 12:17:59 +0800

    初始化路由

--- blog-1/app.js
+++ blog-1/app.js
@@ -1,14 +1,35 @@
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
+
 // 与底层业务有关，与server无关
 const serverHandle = (req, res) => {
+    // 设置返回格式的JSON
     res.setHeader('Content-type', 'application/json')
 
-    const resData = {
-        name: 'Angela',
-        site: 'learn_node_blog',
-        env: process.env.NODE_ENV
+    // 获取path
+    const url = req.url
+    req.path = url.split('?')[0]
+
+    // 处理blog路由
+    const blogData = handleBlogRouter(req, res)
+    if (blogData) {
+        res.end(JSON.stringify(blogData))
+        return
     }
 
-    res.end(JSON.stringify(resData))
+    // 处理user路由
+    const userData = handleUserRouter(req, res)
+    if (userData) {
+        res.end(JSON.stringify(userData))
+        return
+    }
+
+    // 未命中路由，返回404
+    res.writeHead(404, {"Content-type": "text/plain"})
+    res.write("404 Not Found\n")
+    res.end()
 }
 
 module.exports = serverHandle
+
+// process.env.NODE_ENV
